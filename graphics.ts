593877cf26c9559431a1ad5fb2d0d8376e228f4e
francescotili <francesco.tili@gmail.com>
2022-11-24 15:45:00 +0100

    Graphic motor enhancements and fixes

--- graphics.ts
+++ graphics.ts
@@ -18,27 +18,45 @@ export class GraphicMotor {
   private readonly HTML_Title = '<h1>Silberanlage Simulation</h1>';
 
   private readonly graphics = {
-    eol: "<br>",
+    eol: '<br>',
+    // whitespace: '&middot;',
     whitespace: '&nbsp;',
     bath: {
-      top: "&#9487;&#9473;&#9473;&#9491;",
+      label: 'Bad',
+      top: '&#9487;&#9473;&#9473;&#9491;',
       middle: {
-        full: "&#9475;&#9608;&#9608;&#9475;",
-        empty: "&#9475;&nbsp;&nbsp;&#9475;"
+        full: '&#9475;&#9608;&#9608;&#9475;',
+        empty: '&#9475;&nbsp;&nbsp;&#9475;',
       },
-      bottom: "&#9495;&#9473&#9473;&#9499;"
+      bottom: '&#9495;&#9473&#9473;&#9499;',
     },
-    separator: " | ",
-    indicator: " > ",
+    id: {
+      label: 'Nr.',
+    },
+    separator: ' | ',
+    indicator: ' > ',
     crane: {
-      top: "&nbsp;&nbsp;&#9582;",
+      label: 'Krane',
+      top: '&nbsp;&nbsp;&#9582;',
       middle: {
-        full: "&#9608;&#9608;&#9566;",
-        empty: "&nbsp;&nbsp;&#9566;"
+        full: '&#9608;&#9608;&#9566;',
+        empty: '&nbsp;&nbsp;&#9566;',
       },
-      bottom: "&nbsp;&nbsp;&#9583;"
-    }
-  }
+      bottom: '&nbsp;&nbsp;&#9583;',
+    },
+  };
+
+  private readonly lenghts = {
+    white1: 2, // Initial whitespace
+    bathName: 30, // Bath name placeholder
+    sep1: 3, // Separator |
+    bathID: 2, // Bath ID placeholder
+    sep2: 3, // Separator >
+    bathVis: 4, // Width of Bath visualization
+    white2: 2, // Whitespace
+    crane: 3, // Width of crane visualization
+    white3: 5, // Whitespace
+  };
 
   private rendering: string;
 
@@ -51,52 +69,78 @@ export class GraphicMotor {
     this.rendering += this.HTML_Title;
     this.rendering += this.header;
 
-    // Characters for every string
-    const len1 = 2; // Initial whitespace
-    const len2 = 2; // Bath ID placeholder
-    const len3 = 3; // Separator |
-    const len4 = 30; // Bath name placeholder
-    const len5 = 3; // Separator >
-    const len6 = 3; // Width of Bath visualization
-    const len7 = 3; // Width of crane visualization
-    const len8 = 5; // Whitespace
+    // HEADER
+    for (
+      var i = 0;
+      i <
+      this.lenghts.bathName -
+        this.graphics.bath.label.length +
+        this.lenghts.white1;
+      i++
+    ) {
+      this.rendering += this.graphics.whitespace;
+    }
+    this.rendering += this.graphics.bath.label;
+    this.rendering += this.graphics.separator;
+    this.rendering += this.graphics.id.label;
+    for (
+      var i = 0;
+      i <
+      this.lenghts.sep2 +
+        this.lenghts.bathVis +
+        this.lenghts.white2 +
+        (this.lenghts.bathID - this.graphics.id.label.length);
+      i++
+    ) {
+      this.rendering += this.graphics.whitespace;
+    }
+    this.rendering += this.graphics.crane.label;
+    for (
+      var i = 0;
+      i <
+      this.lenghts.white3 +
+        (this.lenghts.crane - this.graphics.crane.label.length);
+      i++
+    ) {
+      this.rendering += this.graphics.whitespace;
+    }
+    // End of line
+    this.rendering += this.graphics.eol;
 
-    // ---------
-    this.rendering += 'Crane example';
-    this.rendering += this.graphics.eol;
-    this.rendering += this.graphics.crane.top;
-    this.rendering += this.graphics.eol;
-    this.rendering += this.graphics.crane.middle.empty;
-    this.rendering += this.graphics.eol;
-    this.rendering += this.graphics.crane.bottom;
-    this.rendering += this.graphics.eol;
-    // ---------
-
-    for (let bathID = 0; bathID < baths.length; bathID++) {
+    // BATHS
+    for (let bathID = 1; bathID < baths.length; bathID++) {
       /* * * * * * *
        * LINE 1/3
        * * * * * * */
       // Whitespace
-      for (var i = 0; i < len1 + len2 + len3 + len4 + len5; i++) {
+      for (
+        var i = 0;
+        i <
+        this.lenghts.white1 +
+          this.lenghts.bathName +
+          this.lenghts.sep1 +
+          this.lenghts.bathID +
+          this.lenghts.sep2;
+        i++
+      ) {
         this.rendering += this.graphics.whitespace;
       }
       // Bath
       this.rendering += this.graphics.bath.top;
       // Whitespace
-      for (var i = 0; i < len6; i++) {
-        this.rendering +=  this.graphics.whitespace;
+      for (var i = 0; i < this.lenghts.white2; i++) {
+        this.rendering += this.graphics.whitespace;
       }
       // Crane
-      if (crane.position === bathID ) {
+      if (crane.position === bathID) {
         this.rendering += this.graphics.crane.top;
-        for (var i = 0; i < len8; i++) {
-          this.rendering +=  this.graphics.whitespace;
+        for (var i = 0; i < this.lenghts.white3; i++) {
+          this.rendering += this.graphics.whitespace;
         }
       } else {
-        for (var i = 0; i < len7+len8; i++) {
-          this.rendering +=  this.graphics.whitespace;
+        for (var i = 0; i < this.lenghts.crane + this.lenghts.white3; i++) {
+          this.rendering += this.graphics.whitespace;
         }
-
       }
       // End of line
       this.rendering += this.graphics.eol;
@@ -105,26 +149,30 @@ export class GraphicMotor {
        * LINE 2/3
        * * * * * * */
       // Whitespace
-      for (var i = 0; i < len1; i++) {
-        this.rendering +=  this.graphics.whitespace;
+      for (var i = 0; i < this.lenghts.white1; i++) {
+        this.rendering += this.graphics.whitespace;
       }
       // Name of the bath
       if (typeof baths[bathID].name !== 'undefined') {
-        for (var i = 0; i < len4 - baths[bathID].name.length; i++) {
-          this.rendering +=  this.graphics.whitespace;
+        for (
+          var i = 0;
+          i < this.lenghts.bathName - baths[bathID].name.length;
+          i++
+        ) {
+          this.rendering += this.graphics.whitespace;
         }
         this.rendering += baths[bathID].name;
       } else {
-        for (var i = 0; i < len4; i++) {
-          this.rendering +=  this.graphics.whitespace;
+        for (var i = 0; i < this.lenghts.bathName; i++) {
+          this.rendering += this.graphics.whitespace;
         }
       }
       // Separator
       if (typeof baths[bathID].name !== 'undefined') {
         this.rendering += this.graphics.separator;
       } else {
-        for (var i = 0; i < len3; i++) {
-          this.rendering +=  this.graphics.whitespace;
+        for (var i = 0; i < this.lenghts.sep1; i++) {
+          this.rendering += this.graphics.whitespace;
         }
       }
       // Bath number
@@ -134,21 +182,28 @@ export class GraphicMotor {
       // Indicator
       this.rendering += this.graphics.indicator;
       // Bath status
-      if (baths[bathID].getStatus() !== BathStatus.Free ) {
+      if (baths[bathID].getStatus() !== BathStatus.Free) {
         this.rendering += this.graphics.bath.middle.full;
       } else {
         this.rendering += this.graphics.bath.middle.empty;
       }
       // Whitespace
-      for (var i = 0; i < len6; i++) {
-        this.rendering +=  this.graphics.whitespace;
+      for (var i = 0; i < this.lenghts.white2; i++) {
+        this.rendering += this.graphics.whitespace;
       }
       // Crane
-      if (crane.position === bathID ) {
-        if ( crane.getStatus() !== CraneStatus.Waiting ) {
-          this.rendering += this.graphics.crane.middle.full
+      if (crane.position === bathID) {
+        if (crane.getStatus() !== CraneStatus.Waiting) {
+          this.rendering += this.graphics.crane.middle.full;
         } else {
-          this.rendering += this.graphics.crane.middle.empty
+          this.rendering += this.graphics.crane.middle.empty;
+        }
+        for (var i = 0; i < this.lenghts.white3; i++) {
+          this.rendering += this.graphics.whitespace;
+        }
+      } else {
+        for (var i = 0; i < this.lenghts.crane + this.lenghts.white3; i++) {
+          this.rendering += this.graphics.whitespace;
         }
       }
       // End of line
@@ -158,18 +213,34 @@ export class GraphicMotor {
        * LINE 3/3
        * * * * * * */
       // Whitespace
-      for (var i = 0; i < len1 + len2 + len3 + len4 + len5; i++) {
-        this.rendering +=  this.graphics.whitespace;
+      for (
+        var i = 0;
+        i <
+        this.lenghts.white1 +
+          this.lenghts.bathName +
+          this.lenghts.sep1 +
+          this.lenghts.bathID +
+          this.lenghts.sep2;
+        i++
+      ) {
+        this.rendering += this.graphics.whitespace;
       }
       // Bath
       this.rendering += this.graphics.bath.bottom;
       // Whitespace
-      for (var i = 0; i < len6; i++) {
-        this.rendering +=  this.graphics.whitespace;
+      for (var i = 0; i < this.lenghts.white2; i++) {
+        this.rendering += this.graphics.whitespace;
       }
       // Crane
-      if (crane.position === bathID ) {
+      if (crane.position === bathID) {
         this.rendering += this.graphics.crane.bottom;
+        for (var i = 0; i < this.lenghts.white3; i++) {
+          this.rendering += this.graphics.whitespace;
+        }
+      } else {
+        for (var i = 0; i < this.lenghts.crane + this.lenghts.white3; i++) {
+          this.rendering += this.graphics.whitespace;
+        }
       }
       // End of line
       this.rendering += this.graphics.eol;
